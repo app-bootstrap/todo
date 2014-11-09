d8c664d02bcb7524f4b73aa64db6dfc0890ea21d
dafeng.xdf <dafeng.xdf@taobao.com>
2014-11-09 19:10:30 +0800

    First Commit

---
+++ test/todo.test.js
@@ -0,0 +1,19 @@
+/* ================================================================
+ * todo by xdf(xudafeng[at]126.com)
+ *
+ * first created at : Sun Nov 09 2014 19:10:08 GMT+0800 (CST)
+ *
+ * ================================================================
+ * Copyright 2013 xdf
+ *
+ * Licensed under the MIT License
+ * You may not use this file except in compliance with the License.
+ *
+ * ================================================================ */
+
+'use strict';
+
+var todo = require('../');
+
+describe('test', function(){
+});
